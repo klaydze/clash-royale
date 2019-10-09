a6c936924ec00f088ce0c935e83aa9c96f486e78
Jessie Semana <jessiesemana@gmail.com>
2019-10-09 17:14:42 +0800

    removed resolver

--- Client/src/app/arenas/arenas-routing.module.ts
+++ Client/src/app/arenas/arenas-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArenasComponent } from './arena-list/arenas.component';
 import { ArenaDetailsComponent } from './arena-details/arena-details.component';
-import { ArenaResolver } from './arena-list/arena-resolver-service';
-import { ArenaDetailsResolver } from './arena-details/arena-details-resolver.service';
 
 const routes: Routes = [
-  { path: '', component: ArenasComponent, resolve: { resolvedArenasData: ArenaResolver } },
-  { path: ':id', component: ArenaDetailsComponent, resolve: { resolvedArenaData: ArenaDetailsResolver } }
+  { path: '', component: ArenasComponent /*, resolve: { resolvedArenasData: ArenaResolver }*/ },
+  { path: ':id', component: ArenaDetailsComponent /*, resolve: { resolvedArenaData: ArenaDetailsResolver }*/ }
 ];
 
 @NgModule({
