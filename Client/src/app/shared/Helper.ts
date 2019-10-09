7366186ced65038d42af16bc653df6d9736b66ba
Jessie Semana <jessiesemana@gmail.com>
2019-10-09 18:13:27 +0800

    update environment file and helper file

--- Client/src/app/shared/Helper.ts
+++ Client/src/app/shared/Helper.ts
@@ -1,13 +1,11 @@
+import { environment } from "src/environments/environment";
+
 export class ApiHelper {
-    static clientRoot = 'https://angular-6-clash-royale.firebaseapp.com/';
-    static clashRoyaleApi = 'https://clashroyale.azurewebsites.net/api'; 
-    static stsAuthority = 'https://application-sts-server.azurewebsites.net/';
+    static clashRoyaleImageApi = environment.clashRoyaleImageApi;
 
-    static clashRoyaleImageApi = 'https://clashroyalestorage.blob.core.windows.net/clashroyaleblob/images'; //'https://clashapi.now.sh/images';  
+    static clientRoot = environment.clientRoot;
+    static clashRoyaleApi = environment.clashRoyaleImageApi;
+    static stsAuthority = environment.stsAuthority;
 
-    // static clientRoot = 'http://localhost:4200/';
-    // static clashRoyaleApi = 'https://localhost:5001/api';
-    // static stsAuthority = 'https://localhost:5003/';
-
-    static clientId = 'clashroyale-client';
+    static clientId = environment.clientId;
 }
--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  clientRoot: 'http://localhost:4200/',
+  clashRoyaleApi: 'https://localhost:5001/api',
+  stsAuthority: 'https://localhost:5003/',
+  clashRoyaleImageApi: 'https://clashroyalestorage.blob.core.windows.net/clashroyaleblob/images',
+  clientId: 'clashroyale-client'
 };
 
 /*
