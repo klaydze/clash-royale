519f0469787499debd6edce8a8c66e3f54c199f7
Jessie Semana <jessiesemana@gmail.com>
2019-10-10 11:26:17 +0800

    point clashRoyaleApi variable to correct environment variable

--- Client/src/app/shared/Helper.ts
+++ Client/src/app/shared/Helper.ts
@@ -1,10 +1,10 @@
-import { environment } from "src/environments/environment";
+import { environment } from 'src/environments/environment';
 
 export class ApiHelper {
     static clashRoyaleImageApi = environment.clashRoyaleImageApi;
 
     static clientRoot = environment.clientRoot;
-    static clashRoyaleApi = environment.clashRoyaleImageApi;
+    static clashRoyaleApi = environment.clashRoyaleApi;
     static stsAuthority = environment.stsAuthority;
 
     static clientId = environment.clientId;
